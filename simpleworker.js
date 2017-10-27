cc2d93418688c4e5bf1c6dfc3037cbd3549e1adf
ᦖєgιт <support@legitsoulja.info>
2017-10-27 00:02:22 -0400

    Update simpleworker.js

--- simpleworker.js
+++ simpleworker.js
@@ -9,99 +9,114 @@
 
 (function (w, d) {
 
-    var worker_handler = w.URL.createObjectURL(new Blob(['(',
-        function () {
-            self.onmessage = function (e) { 
-                return self.postMessage(((eval('(function(func){return func;})(' + (e.data.func) + ')')).apply(null, e.data.args))); 
-            }
-        }.toString(), 
-    ')()'], { type: "application/javascript" }));
-
-    var dth = function(n) {
-        if(n > 0) n = -(Math.abs(n)); 
-        n = 0xFFFFFFFF + n + 1;
-        return n.toString(16).toUpperCase();
-    }
+  var worker_handler = w.URL.createObjectURL(new Blob(['(',
+      function () {
+          self.onmessage = function (e) { 
+              return self.postMessage({index: e.data.index, data: ((eval('(function(func){return func;})(' + (e.data.func) + ')')).apply(null, e.data.args))}); 
+          }
+      }.toString(), 
+  ')()'], { type: "application/javascript" }));
 	
-    var shiftN = function(args, n){
-        for(var i = 0; i < n; i++)
-            args.shift();
-        return args;
-    }
+	this.SimpleWorker = class SimpleWorker {
 	
-    var SimpleWorker = function () {
-        this.workers = {}
-    }
+		constructor(){
+			this.workers = {}
+		}
+		
+		prepare(func){
+			// prepare arguments
+			var args = [];
+			if(arguments.length > 1)
+				Array.prototype.push.apply(args, arguments);
+			args.shift(); // remove func
+			
+			// setup worker pid
+			var pid = (Object.keys(this.workers).length);
+			
+			// create worker object
+			this.workers[("!" + pid)] = {
+				worker: new Worker(worker_handler),
+				func: func,
+				args: args,
+				loads: [],
+				init: function(cb){
+					var len = this.loads.length;
+					this.loads.push(cb)
+					var a = this;
+					this.worker.onmessage = function(e){
+						var i = 0;
+						if(typeof(e) != 'undefined' && typeof(e.data.index) != 'undefined'){
+							if(typeof(a.loads[e.data.index]) != 'undefined') {
+								a.loads[e.data.index](e.data.data);
+								a.loads[e.data.index] = null;
+							}
+						}
+					}
+					this.worker.postMessage({func: (this.func).toString(), args: this.args, index: len });
+				},
+				terminate: function(){
+					this.worker.terminate();
+				}
+			}
+			return pid;
+		}
+		shiftN(args, n){
+		  for(var i = 0; i < n; i++)
+		    args.shift();
+		  return args;
+		}
+		restore(pid, func){
+		  pid = ("!" + pid);
+		  if(typeof(this.workers[pid]) != 'undefined') {
+		    if(func != null)
+		      this.workers[pid].func = func;
+		    var args = [];
+		    if(arguments.length > 2) {
+		      Array.prototype.push.apply(args, arguments);
+		      args = this.shiftN(args, 2);
+		      this.workers[pid].args = args;
+		    }
+		    return;
+		  }
+		  
+		  throw new Error('Unable to find worker with pid ID #' + pid);
+		  
+		}
+		execute(pid, cb, persistent = false){
+		  pid = ("!" + pid);
+		  var a = this;
+			if(typeof(this.workers[pid]) != 'undefined') {
+				if(typeof(cb) == 'function') this.workers[pid].init(function(e){
+				  cb(e);
+					if(!persistent) {
+					  a.workers[pid].terminate();
+					  delete a.workers[pid];
+					}
+				});
+				else throw new Error('Execute requires a callback function as the second argument.');
+			}else throw new Error('Unable to find worker with pid ID #' + pid);
+		}
+		kill(pid){
+		  pid = ("!" + pid)
+		  if(typeof(this.workers[pid]) != 'undefined') {
+		    this.workers[pid].terminate();
+		    delete this.workers[pid];
+		  }
+		}
+		
+		killAll(){
+		  var keys = Object.keys(this.workers);
+		  for(var i in keys) {
+		    this.workers[keys[i]].terminate();
+		    delete this.workers[keys[i]];
+		  }
+		}
+		
+		newInstance(){
+		  return new SimpleWorker();
+		}
 	
-    SimpleWorker.prototype = {
+	}
 
-        prepare: function (func) {
-            if(typeof(func) !== 'function') 
-                throw new Error("Prepare must require a Thread(Function)");
-            var pid = -(Math.abs((Object.keys(this.workers).length) + 1));
-            var args = [];
-            if(arguments.length > 1) {
-                Array.prototype.push.apply(args, arguments);
-                args = shiftN(args, 1);
-            }
-            this.workers[dth(pid)] = {
-                worker: new Worker(worker_handler),
-                args: args,
-                init: function (cb) {
-			/* todo, add custom  event handlers, instead of overwriting */
-		    this.worker.onmessage = function (e) { return cb(e.data); }
-                    this.worker.postMessage({ func: (this.func).toString(), args: this.args});
-                },
-                func: func,
-            }
-            this.pid--;
-            return pid;
-        },
-        restore: function (pid, func) {
-            pid = dth(pid);
-            if (this.workers[pid] != null) {
-                if (func != null)
-                    this.workers[pid].func = func;
-                var args = [];
-                if (arguments.length > 2) {
-                    Array.prototype.push.apply(args, arguments)
-                    args = shiftN(args, 2);
-                    this.workers[pid].args = args;
-                }
-                return;
-            }
-            throw new Error("Thread 0x" + pid + " does not exist.");
-        },
-        execute: function (pid, cb) {
-            pid = dth(pid);
-            if (this.workers[pid] != null)
-                if(typeof(cb) === 'function')
-                    this.workers[pid].init(function (e) { return cb(e); });
-                else throw new Error("Execute must require an async callback.");
-            else throw new Error("Thread 0x" + pid + " does not exist.");	
-        },
-        kill: function (pid) {
-            pid = dth(pid);
-            if (this.workers[pid] != null) {
-                this.workers[pid].worker.terminate();
-                delete this.workers[pid];
-                return;
-            }
-            throw new Error("Could not find thread " + "0x" + pid + ".")
-        },
-        killAll: function () {
-            var keys = Object.keys(this.workers);
-            for (var i in keys) {
-                this.workers[keys[i]].worker.terminate();
-                delete this.workers[keys[i]];
-            }
-        },
-        newInstance(){
-            return new SimpleWorker();
-        }
 
-    }
-
-    w.SimpleWorker = new SimpleWorker();
-
-})(window, document);
+})(this);
